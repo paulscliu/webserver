352c3b63c5bca36bb7a08d7996029aa8de4bf25f
paulscliu <paulsc.liu@gmail.com>
2018-11-19 14:15:10 +0800

    Update server.go

--- server.go
+++ server.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
-	})
-
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.ListenAndServe(":80", nil)
+	fmt.Printf("hello, world\n")
 }
